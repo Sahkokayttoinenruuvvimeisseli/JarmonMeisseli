ce99e01a35f285da921b18621e631dbd46298d8d
Jaska <eikuulu@sulle.t>
2016-09-21 08:33:11 +0300

    Poistettu Serial.print.

--- JarmonMeisseli/JarmonMeisseli.ino
+++ JarmonMeisseli/JarmonMeisseli.ino
@@ -17,16 +17,14 @@ MFRC522::MIFARE_Key key;
 long currentTag = 0;
 
 
-// the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
-    SPI.begin(); // Init SPI bus
-  mfrc522.PCD_Init(); // Init MFRC522
+  SPI.begin();
+  mfrc522.PCD_Init();
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
   if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial() ) {
     delay(50);
@@ -55,17 +53,12 @@ void loop() {
 void Leimaa(long t) {
   digitalWrite(4, HIGH);
   digitalWrite(3, HIGH);
-  Serial.print("Uusi leimaus ");
-  Serial.println(t);
-  
   currentTag = t;
 }
 
 void PoistaNykyinenLeimaus() {
   digitalWrite(4, LOW);
   digitalWrite(3, LOW);
-  Serial.print("Poistetaan leimaus ");
-  Serial.println(currentTag);
   currentTag = 0;
 }
 
